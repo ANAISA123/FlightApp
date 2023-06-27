9d43ed017d85f4dab29f9dbb21dd72a3bf314832
Emmanuel Londoño Gomez <emmalondogo@gmail.com>
2023-06-27 10:43:47 -0500

    correccion de rutas y modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
-import { CommonModule } from '@angular/common';
-import { SharedModule } from './shared/shared.module';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from '../app/shared/header/header.component';
+
 const routes: Routes = [
   {
     path: '',
-    //loadChildren: () => import('./home/home.module').then((module) => module.HomeModule),
+    loadChildren: () => import('./home/home.module').then((module) => module.HomeModule),
   },
 ];
 
