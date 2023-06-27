d9a14102fdf66d5645a43ddf3990ac90985d112c
Ana Isabel <ana.agudelo95@correo.tdea.edu.co>
2023-06-27 17:33:17 -0500

    Creación del componente flightHours

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { ContentComponent } from './content/content.component';
 import { InfoPilotsComponent } from './info-pilots/info-pilots.component';
+import { FlightHoursComponent } from './flight-hours/flight-hours.component';
 
 
 @NgModule({
   declarations: [
     ContentComponent,
-    InfoPilotsComponent
+    InfoPilotsComponent,
+    FlightHoursComponent
   ],
   imports: [
     CommonModule,
